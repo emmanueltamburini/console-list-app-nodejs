9b92dd7053372e98ca95342f3843c3ef86cecdea
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-11-25 08:43:27 -0400

    frist menú has been added

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const { showMenu, pause } = require('./helpers/messages');
+
 require('colors');
 console.clear();
 
-const main = () => {
-    console.log('Hello world');
+const main = async() => {
+    let opt = '';
+    do {
+        opt = await showMenu();
+        console.log({opt});
+
+        if (opt !== '0') await pause();
+    } while (opt !== '0');
 }
 
 main();
---
+++ helpers/messages.js
@@ -0,0 +1,47 @@
+require('colors');
+
+const showMenu = () => {
+    return new Promise(resolve => {
+        console.clear();
+        console.log('=================================='.green);
+        console.log('         Show an option'.green);
+        console.log('=================================='.green);
+    
+        console.log(`${'1.'.green} Create task`);
+        console.log(`${'2.'.green} List all tasks`);
+        console.log(`${'3.'.green} List completed tasks`);
+        console.log(`${'4.'.green} List pending tasks`);
+        console.log(`${'5.'.green} Complete task(s)`);
+        console.log(`${'6.'.green} Delete task(s)`);
+        console.log(`${'0.'.green} Exit \n`);
+    
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question('Choose an option: ', opt => {
+            readline.close();
+            resolve(opt);
+        });
+    });
+}
+
+const pause = () => {
+    return new Promise(resolve => {
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question(`\nPress ${'ENTER'.green}: to continue: \n`, opt => {
+            readline.close();
+            resolve(opt);
+        });
+    })
+}
+
+module.exports = {
+    showMenu,
+    pause
+}
